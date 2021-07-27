140fc6b8c257149acdcff73e431f3736130b7bad
MohamadDawahdeh <>
2021-07-27 16:44:12 +0000

    update

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # gussing-game
 
 
-# LAB - 02
+# LAB - 02 and 03
 
 About Me Project
 
-This project gives an information about me (Mohammad Dawahdeh) and introduce the information by letting the user play a guessing game .
+This project gives an information about me (Mohammad Dawahdeh) and introduce the information by letting the user play a guessing game. This project also asking the user to  to  guess a number and letter and at the end provide the user the number of correct answers.
 
-* Author: Mohammad Dawahdeh.*
+*Author: Mohammad Dawahdeh*
--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ let choo1="n";
 
 if (choo1.localeCompare(choose1.toLowerCase())==0){
 alert("you are smart");
+//console.log("right answer");
 correctAnswer++;
 
 }
@@ -23,70 +24,86 @@ alert("you are not lucky this time")
 let choose2=prompt("Do you think  I am engineer ? y/n");
 let choo2="y";
 if (choo2.localeCompare(choose2.toLowerCase())==0){
+//console.log("right answer");
 
 alert("you are very smart ");
 
 correctAnswer++;
 }
-else
+else{
 alert("you are not lucky this time");
+//console.log("wrong answer");
 
-
+}
 let choose3=prompt("Do you think  I am civil engineer ? y/n");
 let choo3="n";
 if (choo3.localeCompare(choose3.toLowerCase())==0){
+//console.log("right answer");
 
 alert("you are very very smart,I am telecom engineer ");
 correctAnswer++;
 
 }
-else
+else {
 alert("you are not lucky this time, I am telecom engineer");
+//console.log("wrong answer");
 
-
+}
 
 let choose4=prompt("Do you think  7 is my fav number ?");
 let choo4="y";
 if (choo4.localeCompare(choose4.toLowerCase())==0){
 
 alert("you are very smart ");
+//console.log("right answer");
 
 correctAnswer++;
 }
-else
+else{
 alert("you are not lucky this time");
+//console.log("wrong answer");
 
-
+}
 
 let choose5=prompt("Do you think I am living in Amman ? y/n");
 let choo5="n";
 if (choo5.localeCompare(choose5.toLowerCase())==0) {
 alert("you are very very smart, i am living in irbid ");
 correctAnswer++;
+//console.log("right answer");
+
+
 }
-else
+else {
 alert("you are not lucky this time");
+//console.log("wrong answer");
 
-
+}
 
 let counter=1;
 let trueNumber=5;
-while (counter <=4){
+while (counter <=4) {
 
 let guessedNumber=prompt("try to guess a number between 1-10");
 
 if (trueNumber ==guessedNumber) {
 alert("Right guess it is 5");
 correctAnswer++;
+//console.log("right answer");
+
 break;}
-else if (trueNumber <guessedNumber)
+else if (trueNumber <guessedNumber) {
 alert("too high");
-else
+//console.log("wrong answer");
+}
+else{
 alert ("too low");
+//console.log("wrong answer");
 
 counter++;
 }
 
+}
 alert("Ther right guess is 5")
 
 
@@ -102,18 +119,22 @@ let Letter=prompt("guess one of the english letters A,B....Z").toLocaleUpperCase
 if (Letter==possibleChoices[0]||Letter==possibleChoices[1]||Letter==possibleChoices[2]) {
 alert("right guess the true values are C,K,M ");
 correctAnswer++;
+//console.log("right answer");
+
 break; }
-else
+else {
 alert("Try again");
 
 counter++;
+//console.log("wrong answer");
 
 }
 
+}
 alert("The true values are C,K,M ");
 
 
-alert("Thanks for visiting our website "+name+"  Number of correct answers were "+ correctAnswer +"/7")
+alert("Thanks for visiting our website "+name+"  Number of correct answers were "+ correctAnswer +"/7") ;
 
 
 
